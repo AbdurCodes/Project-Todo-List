9707a9ff50b082c378fb62184d99f1845c31c010
AbdurCodes <aravman@gmail.com>
2024-12-13 16:52:03 +0500

    improve the application logic with help from chatgpt

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const toDoContainer = document.querySelector('toDoContainer');
 
-
 class newToDo {
     constructor(title, description, dueDate, priority, notes) {
         this.title = title;
@@ -11,28 +10,47 @@ class newToDo {
         this.isCompleted = false;
     }
 
+
+
     // method that saves newly created todos into localStorage
-    saveToLocalStorage() {
-        // store todo data in js object
-        let todo = {
+    saveToLocalStorage(category) {
+
+        let todos = JSON.parse(localStorage.getItem(category)) || [];
+        todos.push({
             title: this.title,
             description: this.description,
             dueDate: this.dueDate,
             priority: this.priority,
             notes: this.notes,
             isCompleted: this.isCompleted
-        }
-        // convert todo into JSON
-        let jsonTODO = JSON.stringify(todo);
-        // save that json to localStorage
-        localStorage.setItem(this.title, jsonTODO);
+        });
+        localStorage.setItem(category, JSON.stringify(todos));
+
+        // store todo data in js object
+        // let todo = {
+        //     title: this.title,
+        //     description: this.description,
+        //     dueDate: this.dueDate,
+        //     priority: this.priority,
+        //     notes: this.notes,
+        //     isCompleted: this.isCompleted
+        // }
+        // // convert todo into JSON
+        // let jsonTODO = JSON.stringify(todo);
+        // // save that json to localStorage
+        // localStorage.setItem(this.title, jsonTODO);
     }
 
+
+
     // method that loads to-dos from the localStorage to display on page
-    retrieveFromLocalStorage() {
-        let jsonFromLS = localStorage.getItem(this.title);
-        let json_to_obj = JSON.parse(jsonFromLS);
-        console.log(json_to_obj);
+    static retrieveFromLocalStorage(category) {
+
+        return JSON.parse(localStorage.getItem(category)) || [];
+
+        // let jsonFromLS = localStorage.getItem(this.title);
+        // let json_to_obj = JSON.parse(jsonFromLS);
+        // console.log(json_to_obj);
     }
 
     setToDoComplete() {
@@ -54,6 +72,49 @@ class newToDo {
 
 
 
+
+// categories of to-dos
+// given: life, work, education
+// create a new category
+
+// let todoCategories = {
+//     life: [],
+//     work: [],
+//     education: [],
+//     createNewCat: function (catName) {
+//         if (!this[catName]) {
+//             this[catName] = [];
+//         }
+//         return this[catName];
+//     }
+// }
+
+
+class TodoCategories  {
+    constructor() {
+        this.categories = {
+            life: [],
+            work: [],
+            education: [],
+        }
+    }
+
+    createNewCat(catName) {
+        if (!this.categories[catName]) {
+            this.categories[catName] = [];
+        }
+        return this.categories[catName];
+    }
+
+    addToDoToCategory (category, todo) {
+        if (!this.categories[category]) {
+            this.createNewCat(category);
+        }
+        this.categories[category].push(todo);
+    }
+}
+
+
 let todo1 = new newToDo(
     "example todo",
     "This is an example todo only",
@@ -63,34 +124,16 @@ let todo1 = new newToDo(
 )
 
 
-
-// categories of to-dos
-// given: life, work, education
-// create a new category
-
-let todoCategories = {
-    life: [],
-    work: [],
-    education: [],
-    createNewCat: function (catName) {
-        if (!this[catName]) {
-            this[catName] = [];
-        }
-        return this[catName];
-    }
-}
-
-
-
-
-
+let todoCategories = new TodoCategories();
+todoCategories.addToDoToCategory("life", todo1);
+todoCategories.addToDoToCategory("word", todo2);
 
 // adding a to-do into a certain category
-todoCategories.life.push(todo1);
-todoCategories.work.push(todo1);
-todoCategories.education.push(todo1);
-todoCategories.createNewCat("Sports").push(todo1);
-todoCategories.createNewCat("Travel").push(todo1);
+// todoCategories.life.push(todo1);
+// todoCategories.work.push(todo1);
+// todoCategories.education.push(todo1);
+// todoCategories.createNewCat("Sports").push(todo1);
+// todoCategories.createNewCat("Travel").push(todo1);
 
 
 
